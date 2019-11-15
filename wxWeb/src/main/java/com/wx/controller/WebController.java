72a00e9481f0866188d9ade1bd68744d8562d1a5
zhong.wan <zhong.wan@timanetworks.com>
2019-11-15 18:50:29 +0800

    init

--- wxWeb/src/main/java/com/wx/controller/WebController.java
+++ wxWeb/src/main/java/com/wx/controller/WebController.java
@@ -3,18 +3,16 @@ package com.wx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@RequestMapping("web")
+@RequestMapping("webController")
 @Controller
 public class WebController {
 
-    @GetMapping(value = "/test")
-    @ResponseBody
+    @GetMapping(value = "/hello")
     public ModelAndView test(){
-        ModelAndView mv = new ModelAndView("test");
+        ModelAndView mv = new ModelAndView("hello");
         return mv;
     }
 
